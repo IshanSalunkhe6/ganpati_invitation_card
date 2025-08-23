82e88ae6fc95e47968466c8fcb3f14310a6c9d07
IshanSalunkhe6 <salunkhe.ishan06@gmail.com>
2025-08-23 16:48:07 +0530

    Update

--- app/layout.js
+++ app/layout.js
@@ -3,14 +3,16 @@ import "./globals.css";
 export const metadata = {
   title: "Ganesh Darshan 2025",
   description: "Ganesh Darshan Invitation Website",
+  icons: {
+    icon: "/logo.jpg", // Path to your logo in the public folder
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Add Ganpati logo as favicon */}
-        <link rel="icon" href="/ganpati-logo.jpg" type="image/jpeg" sizes="32x32" />
+        <link rel="icon" href="/logo.jpg" type="image/jpg" />
       </head>
       <body className="bg-gradient-to-b from-orange-50 to-red-100 text-gray-900">
         {children}
